84dd13fa0108241d65fba0a7618750efb63c151a
sushimitsu1 <sushimitsu1@gmail.com>
2026-08-04 15:20:21 -0700

    Publish five new Nolan bedtime stories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nolan-library-v45';
+const CACHE='nolan-library-v46';
 const CORE=['./','index.html','styles.css','app.js','config/narration-voices.json','manifest.webmanifest','assets/icons/icon-192.png','assets/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
